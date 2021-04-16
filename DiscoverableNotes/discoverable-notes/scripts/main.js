835b9f1181d74bb86c7da3c19dffe833a9594c27
Kris Barber <kristopher.p.barber@gmail.com>
2021-04-16 09:15:48 -0400

    basic functionality

--- DiscoverableNotes/discoverable-notes/scripts/main.js
+++ DiscoverableNotes/discoverable-notes/scripts/main.js
@@ -1,21 +1,20 @@
 Hooks.once('init', () => {
-
     let originalRefresh = Note.prototype.refresh;
-        Note.prototype.refresh = function(){
+    Note.prototype.refresh = function(){
         this.position.set(this.data.x, this.data.y);
         this.controlIcon.border.visible = this._hover;
         this.tooltip.visible = this._hover;
-        this.visible = this.entry ? this.entry.hasPerm(game.user, 0) : true;
+        this.visible = this.entry ? this.entry.hasPerm(game.user, game.settings.get("discoverable-notes", "PickupPermission")) : true;
         return this;
     }
-    
-    /*
-    if( game.settings.get("Discoverable-Notes", "MinimumInteractionPermissions") < 2 ) {
-        
+
+    Note.prototype._canView = function(user){
+        return this.entry ? this.entry.hasPerm(game.user, game.settings.get("discoverable-notes", "PickupPermission")) : false
     }
 
 
-    if( game.settings.get("Discoverable-Notes", "InteractionDistance") > 0 ) {
+    /*
+    if( game.settings.get("discoverable-notes", "InteractionDistance") > 0 ) {
         
       let originalMethod = DoorControl.prototype._onMouseDown;
       DoorControl.prototype._onMouseDown = function(event) {
@@ -31,7 +30,7 @@ Hooks.once('init', () => {
           let dist = getManhattanBetween(this, getTokenCenter(character));
           let gridSize = canvas.dimensions.size;
   
-          if ( (dist / gridSize) > game.settings.get("Discoverable-Notes", "InteractionDistance") ) {
+          if ( (dist / gridSize) > game.settings.get("discoverable-notes", "InteractionDistance") ) {
             var tokenName = getCharacterName(character);
             if (tokenName) iteractionFailNotification("Note not within " + tokenName + "'s reach" );
             else iteractionFailNotification("Note not in reach" );
--- DiscoverableNotes/discoverable-notes/scripts/settings.js
+++ DiscoverableNotes/discoverable-notes/scripts/settings.js
@@ -1,6 +1,6 @@
-/*
+
 Hooks.once("init", () => {
-    game.settings.register("Discoverable-Notes", "InteractionDistance", {
+    game.settings.register("discoverable-notes", "InteractionDistance", {
         name: "Interaction Distance Limit",
         hint: "The max distance that a token can interact with a note. Setting this to 0 disables the limit.",
         scope: "world",
@@ -14,21 +14,7 @@ Hooks.once("init", () => {
         }
     });
 
-    game.settings.register("Discoverable-Notes", "PickupPermission", {
-        name: "Journal Permission on Pickup",
-        hint: "The permission that the  disable interaction with double tap.",
-        scope: "world",
-        config: true,
-        default: 200,
-        type: Number,
-        range: {
-            min: 0,
-            max: 750,
-            step: 50
-        }
-    });
-
-    game.settings.register("Discoverable-Notes", "PartyPickup", {
+    game.settings.register("discoverable-notes", "PartyPickup", {
         name: "Pickup Notes for Whole Party",
         hint: "Whether a picked up note is added to the journal for all players.",
         scope: "world",
@@ -37,19 +23,18 @@ Hooks.once("init", () => {
         type: Boolean
     });
 
-    game.settings.register("Discoverable-Notes", "MinimumInteractionPermissions", {
+    game.settings.register("discoverable-notes", "PickupPermission", {
         name: "Minimum Permission to Pick Up Note",
         hint: 'The minimum permission that a player needs to have to pick up Notes. "none" is the recommendation.',
         scope: "world",
         config: true,
-        default: "none",
-        type: Number,
+        default: "NONE",
+        type: String,
         choices: {
-            "none": 0,
-            "limited" : 1,
-            "observer" : 2,
-            "owner" : 3
+            "NONE": "none",
+            "LIMITED": "limited",
+            "OBSERVER": "observer",
+            "OWNER": "owner"
         }
     });
 });
-*/
